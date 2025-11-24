d751eee582a9dca43c10adf78aff02969c41c269
Corey Schulz <cschulz@hey.com>
2025-11-24 12:55:02 -0700

    blank xcode proj

---
+++ liquid-glass-terminal/liquid-glass-terminal/liquid_glass_terminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  liquid_glass_terminalApp.swift
+//  liquid-glass-terminal
+//
+//  Created by Corey Schulz on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct liquid_glass_terminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
